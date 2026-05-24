84bb25a3cdfa119a7b78943c0a8230fd9fbf865a
stevenandz-glitch <stevenandz92491@gmail.com>
2026-05-24 10:08:54 -0400

    made some changes

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,7 @@
 # Uncomment the imports below before you add the function code
 # import requests
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,12 +11,35 @@ backend_url = os.getenv(
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
+    
+def get_request(endpoint, **kwargs):
+    params = ""
+    if(kwargs):
+        for key,value in kwargs.items():
+            params=params+key+"="+value+"&"
 
-# def get_request(endpoint, **kwargs):
-# Add code for get requests to back end
+    request_url = backend_url+endpoint+"?"+params
 
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+
+
+def analyze_review_sentiments(text):
+  request_url = sentiment_analyzer_url+"analyze/"+text
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except Exception as err:
+        print(f"Unexpected {err=}, {type(err)=}")
+        print("Network exception occurred")
+
 # Add code for retrieving sentiments
 
 # def post_review(data_dict):
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -91,7 +91,14 @@ def get_cars(request):
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
-# def get_dealerships(request):
+def get_dealerships(request, state="All"):
+    if(state == "All"):
+        endpoint = "/fetchDealers"
+    else:
+        endpoint = "/fetchDealers/"+state
+    dealerships = get_request(endpoint)
+    return JsonResponse({"status":200,"dealers":dealerships})
+
 # ...
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
