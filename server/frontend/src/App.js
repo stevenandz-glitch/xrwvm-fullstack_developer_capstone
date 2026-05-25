a351ac1f100ad160c6921da41ebe3bfb2dec0ec0
stevenandz-glitch <stevenandz92491@gmail.com>
2026-05-25 14:05:17 -0400

    Added reviews things

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import Dealers from "./components/Dealers/Dealers"
 import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path="/login" element={<LoginPanel/>}/>
       <Route path="/dealers" element={<Dealers/>}/>
       <Route path="/dealer/:id" element={<Dealer/>}/>
+      <Route path="/postreview/:id" element={<PostReview/>}/>
     </Routes>
   );
 }
