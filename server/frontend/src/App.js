a18537001abe9ca89656d5b338895ea54bb563f0
stevenandz-glitch <stevenandz92491@gmail.com>
2026-05-25 23:59:51 -0400

    Fixed routing issues

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,7 @@
 import LoginPanel from "./components/Login/Login"
 import Dealers from "./components/Dealers/Dealers"
 import Dealer from "./components/Dealers/Dealer"
+import Register from "./components/Register/Register"
 import PostReview from "./components/Dealers/PostReview"
 import { Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel/>}/>
       <Route path="/dealers" element={<Dealers/>}/>
+      <Route path="/register" element={<Register/>}/>
       <Route path="/dealer/:id" element={<Dealer/>}/>
       <Route path="/postreview/:id" element={<PostReview/>}/>
     </Routes>
